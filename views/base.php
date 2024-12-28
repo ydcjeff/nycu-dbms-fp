da6185e48e01943943f4e8bbf5ca3f3cd3413b27
Jeff Yang <n2ya@duck.com>
2024-12-28 12:28:33 +0800

    usable compare ui (#10)
    fix #7

--- views/base.php
+++ views/base.php
@@ -1,9 +1,9 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title><?php echo $title . 'University Ranking System' ?></title>
+  <title><?php echo $title ?? '' . 'University Ranking System' ?></title>
   <link
     rel="stylesheet"
     href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css"
@@ -12,6 +12,12 @@
   <script>
     tailwind.config = { corePlugins: { preflight: false } };
   </script>
+  <style>
+    /* https://picocss.com/docs/css-variables */
+    :root {
+      --pico-font-size: 1rem;
+    }
+  </style>
 </head>
 <body class="container">
   <header class="flex justify-between">
--- views/compare.php
+++ views/compare.php
@@ -1,33 +1,25 @@
 <?php
-// get all necessary data
-$uni1_query = $_GET['uni1'] || 1;
-$uni2_query = $_GET['uni2'] || 2;
-
-$rc = new RankingsController();
-$universities = $rc->get_all_universities();
-$uni1_ranks = $rc->find_by_institute_id(1);
-$uni2_ranks = $rc->find_by_institute_id(2);
-
+// output buffer on
 ob_start();
+// HTML templating below
 ?>
 
-<!-- HTML templating here -->
-<div class="grid grid-cols-6 gap-x-2">
-  <!-- empty div to take the space -->
-  <div></div>
-  <form action="/index.php" method="get" class="col-start-3 col-span-4">
+<div class="grid grid-cols-2 gap-x-2">
+  <form action="/" method="get" class="col-start-2">
     <div class="grid grid-cols-2 gap-x-4">
+      <!-- TODO: replace a big list with search box -->
       <select name="uni1" id="uni1">
-        <?php foreach($universities as $uni): ?>
-          <option value="<?php echo $uni['id'] ?>">
-            <?php print_r($uni['name']); ?>
+        <?php foreach ($universities as $uni): ?>
+          <option value="<?php echo $uni['id'] ?>" <?php echo $uni['id'] === $uni1_query ? 'selected' : '' ?>>
+            <?php echo $uni['name'] ?>
           </option>
         <?php endforeach; ?>
       </select>
+      <!-- TODO: replace a big list with search box -->
       <select name="uni2" id="uni2">
-        <?php foreach($universities as $uni): ?>
-          <option value="<?php echo $uni['id'] ?>">
-            <?php print_r($uni['name']); ?>
+        <?php foreach ($universities as $uni): ?>
+          <option value="<?php echo $uni['id'] ?>" <?php echo $uni['id'] === $uni2_query ? 'selected' : '' ?>>
+            <?php echo $uni['name'] ?>
           </option>
         <?php endforeach; ?>
       </select>
@@ -35,90 +27,142 @@ ob_start();
     <button type="submit">Search</button>
   </form>
 
-  <div class="justify-self-center col-span-2">
-    <?php foreach($uni1_ranks as $rank): ?>
-      <details>
+  <div class="col-span-full">
+    <?php foreach ($uni1_ranks as $idx => $rank): ?>
+      <details open>
         <summary><?php echo $rank['ranked_year'] ?></summary>
-        <ul>
-          <li>World Rank</li>
-          <li>academic_reputation_score</li>
-          <li>academic_reputation_rank</li>
+        <table class="compare">
+          <tbody>
+            <tr>
+              <!-- add more description in th text content -->
+              <th class="w-[50%]">World rank</th>
+              <td class="w-[25%] text-center"><?php echo $rank['world_rank'] ?></td>
+              <td class="w-[25%] text-center"><?php echo $uni2_ranks[$idx]['world_rank'] ?></td>
+            </tr>
 
-          <li>employer_reputation_score</li>
-          <li>employer_reputation_rank</li>
+            <tr>
+              <th class="w-[50%]">Academic Reputation Score</th>
+              <td class="w-[25%] text-center"><?php echo $rank['academic_reputation_score'] ?></td>
+              <td class="w-[25%] text-center"><?php echo $uni2_ranks[$idx]['academic_reputation_score'] ?></td>
+            </tr>
 
-          <!-- TODO: change as DB cols -->
-          <li>employer_reputation_score</li>
-          <li>employer_reputation_rank</li>
+            <tr>
+              <th class="w-[50%]">Academic Reputation Rank</th>
+              <td class="w-[25%] text-center"><?php echo $rank['academic_reputation_rank'] ?></td>
+              <td class="w-[25%] text-center"><?php echo $uni2_ranks[$idx]['academic_reputation_rank'] ?></td>
+            </tr>
 
-          <li>employer_reputation_score</li>
-          <li>employer_reputation_rank</li>
+            <tr>
+              <th class="w-[50%]">Employer Reputation Score</th>
+              <td class="w-[25%] text-center"><?php echo $rank['employer_reputation_score'] ?></td>
+              <td class="w-[25%] text-center"><?php echo $uni2_ranks[$idx]['employer_reputation_score'] ?></td>
+            </tr>
 
-          <li>employer_reputation_score</li>
-          <li>employer_reputation_rank</li>
+            <tr>
+              <th class="w-[50%]">Employer Reputation Rank</th>
+              <td class="w-[25%] text-center"><?php echo $rank['employer_reputation_rank'] ?></td>
+              <td class="w-[25%] text-center"><?php echo $uni2_ranks[$idx]['employer_reputation_rank'] ?></td>
+            </tr>
 
-          <li>employer_reputation_score</li>
-          <li>employer_reputation_rank</li>
-        </ul>
+            <tr>
+              <th class="w-[50%]">Faculty Student Score</th>
+              <td class="w-[25%] text-center"><?php echo $rank['faculty_student_score'] ?></td>
+              <td class="w-[25%] text-center"><?php echo $uni2_ranks[$idx]['faculty_student_score'] ?></td>
+            </tr>
+
+            <tr>
+              <th class="w-[50%]">Faculty Student Rank</th>
+              <td class="w-[25%] text-center"><?php echo $rank['faculty_student_rank'] ?></td>
+              <td class="w-[25%] text-center"><?php echo $uni2_ranks[$idx]['faculty_student_rank'] ?></td>
+            </tr>
+
+            <tr>
+              <th class="w-[50%]">Citations per Faculty Score</th>
+              <td class="w-[25%] text-center"><?php echo $rank['citations_per_faculty_score'] ?></td>
+              <td class="w-[25%] text-center"><?php echo $uni2_ranks[$idx]['citations_per_faculty_score'] ?></td>
+            </tr>
+
+            <tr>
+              <th class="w-[50%]">Citations per Faculty Rank</th>
+              <td class="w-[25%] text-center"><?php echo $rank['citations_per_faculty_rank'] ?></td>
+              <td class="w-[25%] text-center"><?php echo $uni2_ranks[$idx]['citations_per_faculty_rank'] ?></td>
+            </tr>
+
+            <tr>
+              <th class="w-[50%]">International Faculty Score</th>
+              <td class="w-[25%] text-center"><?php echo $rank['intl_faculty_score'] ?></td>
+              <td class="w-[25%] text-center"><?php echo $uni2_ranks[$idx]['intl_faculty_score'] ?></td>
+            </tr>
+
+            <tr>
+              <th class="w-[50%]">International Faculty Rank</th>
+              <td class="w-[25%] text-center"><?php echo $rank['intl_faculty_rank'] ?></td>
+              <td class="w-[25%] text-center"><?php echo $uni2_ranks[$idx]['intl_faculty_rank'] ?></td>
+            </tr>
+
+            <tr>
+              <th class="w-[50%]">International Student Score</th>
+              <td class="w-[25%] text-center"><?php echo $rank['intl_stu_score'] ?></td>
+              <td class="w-[25%] text-center"><?php echo $uni2_ranks[$idx]['intl_stu_score'] ?></td>
+            </tr>
+
+            <tr>
+              <th class="w-[50%]">International Student Rank</th>
+              <td class="w-[25%] text-center"><?php echo $rank['intl_stu_rank'] ?></td>
+              <td class="w-[25%] text-center"><?php echo $uni2_ranks[$idx]['intl_stu_rank'] ?></td>
+            </tr>
+
+            <tr>
+              <th class="w-[50%]">International Research Network Score</th>
+              <td class="w-[25%] text-center"><?php echo $rank['intl_research_network_score'] ?></td>
+              <td class="w-[25%] text-center"><?php echo $uni2_ranks[$idx]['intl_research_network_score'] ?></td>
+            </tr>
+
+            <tr>
+              <th class="w-[50%]">International Research Network Rank</th>
+              <td class="w-[25%] text-center"><?php echo $rank['intl_research_network_rank'] ?></td>
+              <td class="w-[25%] text-center"><?php echo $uni2_ranks[$idx]['intl_research_network_rank'] ?></td>
+            </tr>
+
+            <tr>
+              <th class="w-[50%]">Employment Outcome Score</th>
+              <td class="w-[25%] text-center"><?php echo $rank['employment_outcome_score'] ?></td>
+              <td class="w-[25%] text-center"><?php echo $uni2_ranks[$idx]['employment_outcome_score'] ?></td>
+            </tr>
+
+            <tr>
+              <th class="w-[50%]">Employment Outcome Rank</th>
+              <td class="w-[25%] text-center"><?php echo $rank['employment_outcome_rank'] ?></td>
+              <td class="w-[25%] text-center"><?php echo $uni2_ranks[$idx]['employment_outcome_rank'] ?></td>
+            </tr>
+
+            <tr>
+              <th class="w-[50%]">Sustainability Score</th>
+              <td class="w-[25%] text-center"><?php echo $rank['sustainability_score'] ?></td>
+              <td class="w-[25%] text-center"><?php echo $uni2_ranks[$idx]['sustainability_score'] ?></td>
+            </tr>
+
+            <tr>
+              <th class="w-[50%]">Sustainability Rank</th>
+              <td class="w-[25%] text-center"><?php echo $rank['sustainability_rank'] ?></td>
+              <td class="w-[25%] text-center"><?php echo $uni2_ranks[$idx]['sustainability_rank'] ?></td>
+            </tr>
+
+            <tr>
+              <th class="w-[50%]">Overall</th>
+              <td class="w-[25%] text-center"><?php echo $rank['overall'] ?></td>
+              <td class="w-[25%] text-center"><?php echo $uni2_ranks[$idx]['overall'] ?></td>
+            </tr>
+          </tbody>
+        </table>
       </details>
-    <?php endforeach; ?>
-  </div>
-
-  <div class="justify-self-center col-span-2">
-    <?php foreach($uni1_ranks as $rank): ?>
-      <ul>
-        <li><?php echo $rank['world_rank'] ?></li>
-        <li><?php echo $rank['academic_reputation_score'] ?></li>
-        <li><?php echo $rank['academic_reputation_rank'] ?></li>
-
-        <li><?php echo $rank['employer_reputation_score'] ?></li>
-        <li><?php echo $rank['employer_reputation_rank'] ?></li>
-
-        <!-- TODO: change as DB cols -->
-        <li>employer_reputation_score</li>
-        <li>employer_reputation_rank</li>
-
-        <li>employer_reputation_score</li>
-        <li>employer_reputation_rank</li>
-
-        <li>employer_reputation_score</li>
-        <li>employer_reputation_rank</li>
-
-        <li>employer_reputation_score</li>
-        <li>employer_reputation_rank</li>
-      </ul>
-    <?php endforeach; ?>
-  </div>
-
-  <div class="justify-self-center col-span-2">
-    <?php foreach($uni2_ranks as $rank): ?>
-      <ul>
-        <li><?php echo $rank['world_rank'] ?></li>
-        <li><?php echo $rank['academic_reputation_score'] ?></li>
-        <li><?php echo $rank['academic_reputation_rank'] ?></li>
-
-        <li><?php echo $rank['employer_reputation_score'] ?></li>
-        <li><?php echo $rank['employer_reputation_rank'] ?></li>
-
-        <!-- TODO: change as DB cols -->
-        <li>employer_reputation_score</li>
-        <li>employer_reputation_rank</li>
-
-        <li>employer_reputation_score</li>
-        <li>employer_reputation_rank</li>
-
-        <li>employer_reputation_score</li>
-        <li>employer_reputation_rank</li>
-
-        <li>employer_reputation_score</li>
-        <li>employer_reputation_rank</li>
-      </ul>
     <?php endforeach; ?>
   </div>
 </div>
 
-<!-- substitute the above html template -->
 <?php
+// get the current output buffer and assign to $body which is used in base.php
+// and clean the output buffer
 $body = ob_get_clean();
 require_once __DIR__ . '/base.php';
 ?>
